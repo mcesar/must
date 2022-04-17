404ff51cd6c912767237b6fb24ff822070be1d8b
Andrew Rodionoff <andviro@gmail.com>
2022-04-18 01:06:35 +0300

    rewritten using guard error type idea (https://tech.even.in/posts/go118-error-handling/), added HandleFunc

--- must.go
+++ must.go
@@ -9,12 +9,19 @@
 //  }
 package must
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type wrappedError struct{ error }
+
+func (w wrappedError) Unwrap() error { return w.error }
 
 // Do returns a or panics if err != nil
 func Do[A any](a A, err error) A {
 	if err != nil {
-		panic(err)
+		panic(wrappedError{err})
 	}
 	return a
 }
@@ -22,39 +29,54 @@ func Do[A any](a A, err error) A {
 // Do0 panics if err != nil
 func Do0(err error) {
 	if err != nil {
-		panic(err)
+		panic(wrappedError{err})
 	}
 }
 
 // Do2 returns a and b or panics if err != nil
 func Do2[A, B any](a A, b B, err error) (A, B) {
 	if err != nil {
-		panic(err)
+		panic(wrappedError{err})
 	}
 	return a, b
 }
 
-// Handle sets err to recovered value if it is an error
-func Handle(err *error) {
-	v := recover()
-	if v == nil {
-		return
-	}
-	if err == nil {
-		panic(v)
-	}
-	if e, ok := v.(error); ok && e != nil {
-		*err = e
-		return
-	}
-	panic(v)
+// Handle sets pErr to recovered value if it is an error emitted inside of a Do call
+func Handle(pErr *error) {
+	e := recover()
+	handle(pErr, e)
 }
 
-// Handlef sets err to recovered value if it is an error,
-// wrapped according to the formatting string specified
-func Handlef(err *error, str string) {
-	Handle(err)
-	if err != nil && *err != nil {
-		*err = fmt.Errorf(str, *err)
+func handle(pErr *error, e interface{}) {
+	switch {
+	case e == nil:
+		return
+	default:
+		var errTyped wrappedError
+		if eError, ok := e.(error); ok && errors.As(eError, &errTyped) {
+			if pErr != nil {
+				*pErr = errTyped.error
+			}
+			return
+		}
 	}
+	panic(e)
+}
+
+// Handlef sets err to recovered value if it is an error, wrapped according to
+// the formatting string specified
+func Handlef(pErr *error, str string) {
+	e := recover()
+	handle(pErr, e)
+	if pErr != nil && *pErr != nil {
+		*pErr = fmt.Errorf(str, *pErr)
+	}
+}
+
+// HandleFunc recovers error and passes it to the handler function
+func HandleFunc(f func(err error)) {
+	var err error
+	e := recover()
+	handle(&err, e)
+	f(err)
 }
